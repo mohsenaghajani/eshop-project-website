e027476bf164db0dbaf7848357bcaa317a4d9f36
mohsen <mohsenaghajani020@gmail.com>
2024-02-24 08:58:36 +0330

    add avrage rating and product count in list view

--- product_module/views.py
+++ product_module/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.db.models import Avg
+
 # Create your views here.
 
 
 def products_list(request):
-    products = Product.objects.filter(is_active=True)
+    products = Product.objects.filter(is_active=True).order_by('-price')
+    number_of_products = products.count()
+    rating_avg = products.aggregate(Avg('rate'))
     context = {
-        'products': products
+        'products': products,
+        'product_count': number_of_products,
+        'rating_avg': rating_avg
     }
     return render(request, 'product_module/product_List.html', context)
 
