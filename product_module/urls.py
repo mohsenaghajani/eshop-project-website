bb3ab1608aed7dabb71e5bba073c8fa597a0cb17
mohsen <mohsenaghajani020@gmail.com>
2024-03-25 13:40:53 +0330

    add brand component and price filter

--- product_module/urls.py
+++ product_module/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.ProductList.as_view(), name='products-list'),
     path('<slug:slug>', views.ProductDetail.as_view(), name='products-detail'),
     path('cat/<cat>/', views.ProductList.as_view(), name='category-list'),
+    path('brand/<brand>/', views.ProductList.as_view(), name='brand-list'),
 
 ]
--- product_module/views.py
+++ product_module/views.py
@@ -1,9 +1,11 @@
+from django.http import HttpRequest
 from django.shortcuts import render, get_object_or_404
 
 from article.models import Category
-from .models import Product
+from .models import Product, Brand
 from django.views.generic import ListView, DetailView
-from django.db.models import Avg
+from django.db.models import Avg, Count
+
 
 # Create your views here.
 
@@ -13,17 +15,34 @@ class ProductList(ListView):
     model = Product
     context_object_name = 'products'
     ordering = ['final_price']
-    paginate_by = 1
+    paginate_by = 2
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductList, self).get_context_data()
+        query = Product.objects.all()
+        product: Product = query.order_by('-price').first()
+        db_max_price = product.price or 100000
+        context['db_max_price'] = db_max_price
+        context['start_price'] = self.request.GET.get('start_price') or 0
+        context['end_price'] = self.request.GET.get('end_price') or 100000
+        return context
 
     def get_queryset(self):
-        base_query = super(ProductList, self).get_queryset()
+        query = super(ProductList, self).get_queryset()
         category_title = self.kwargs.get('cat')
+        brand_name = self.kwargs.get('brand')
+        request: HttpRequest = self.request
+        start_price = request.GET.get('start_price')
+        end_price = request.GET.get('end_price')
+        if start_price is not None:
+            query = query.filter(price__gte=start_price)
+        if end_price is not None:
+            query = query.filter(price__lte=end_price)
         if category_title is not None:
-            products = base_query.filter(is_active=True, category__url_title__iexact=category_title)
-            return products
-        products = base_query.filter(is_active=True)
-        return products
-
+            query = query.filter(is_active=True, category__url_title__iexact=category_title)
+        if brand_name is not None:
+            query = query.filter(is_active=True, brand__name__iexact=brand_name)
+        return query
 
 
 class ProductDetail(DetailView):
@@ -39,4 +58,9 @@ def category_component(request):
     return render(request, 'component/product_category_component.html', context)
 
 
-
+def brand_component(request):
+    brands = Brand.objects.all().annotate(product_count=Count('product'))
+    context = {
+        'brands': brands
+    }
+    return render(request, 'component/brand_component.html', context)
